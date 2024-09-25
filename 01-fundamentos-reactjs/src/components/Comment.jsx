a057706a03d0fca6564b7dbaadfa3c1ab6cb9161
Guilherme Pessoa <guipessoagith@gmail.com>
2024-09-25 09:37:41 -0300

    Delete Comments

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -3,7 +3,12 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react'
 
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/guicpessoa.png"/>
@@ -16,7 +21,7 @@ export function Comment({ content }) {
                             <time title="16 de Setembro às 11:50h" dateTime="2024-09-16 11:51:00">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
